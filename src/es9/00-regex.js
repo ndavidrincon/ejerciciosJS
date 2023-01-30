371d23d01bde88969612fba3f3aec9fc0155e781
ndavidrincon <digitalonly.agency@gmail.com>
2023-01-30 04:34:05 -0500

    Caracteristicas agregadas de ECMAScript 9

---
+++ src/es9/00-regex.js
@@ -0,0 +1,5 @@
+// Mejora para capturar en grupos valores que estan dentro de una exprecion regular
+
+const rejex = /(\d{4})-(\d{2})-(\d{2})/;
+const matchers = regex.exec("2022-01-01");
+console.table(matchers);
